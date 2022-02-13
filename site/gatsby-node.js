408d50615ca0a22f43939071ebd3c755cf30b581
Generosa Litton <glcodeworks@gmail.com>
2022-02-13 13:41:46 -0800

    more node apis

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,12 +1,17 @@
+const fetch = require('node-fetch');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+
 const authors = require('./src/data/authors.json');
 const books = require('./src/data/books.json');
 
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
   const { createNode, createTypes } = actions;
 
+  // createTypes helps create a foreign key relationship
+  // Below is a graphql schema
   createTypes(`
     type Author implements Node {
-      books: [Book!]! @link(from: "slug" by: "author.slug")
+      books: [Book!]! @link(from: "slug" by: "author.slug") 
     }
 
     type Book implements Node {
@@ -56,4 +61,28 @@ exports.createPages = ({ actions }) => {
       },
     },
   });
+};
+// resolver is a graphQL term for a function that returns data for a given field
+exports.createResolvers = ({
+  action,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  // create a resolver object that loosely matches the schema
+  const resolvers = {
+    Book: {
+      buyLink: {
+        type: 'String',
+        resolve: (source) =>
+          `https://www.powells.com/searchresults?keyword=${source.isbn}`,
+      },
+      cover: {
+        type: 'File',
+      },
+    },
+  };
+  createResolvers(resolvers);
 };
