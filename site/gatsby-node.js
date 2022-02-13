a9aa98c32f9db35aa447ed59a1298fca8819cd52
Generosa Litton <glcodeworks@gmail.com>
2022-02-13 12:41:14 -0800

    added book data type

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -2,7 +2,17 @@ const authors = require('./src/data/authors.json');
 const books = require('./src/data/books.json');
 
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-  const { createNode } = actions;
+  const { createNode, createTypes } = actions;
+
+  createTypes(`
+    type Author implements Node {
+      books: [Book!]! @link(from: "slug" by: "author.slug")
+    }
+
+    type Book implements Node {
+      author: Author! @link(from: "author" by: "slug")
+    }
+  `);
 
   authors.forEach((author) => {
     createNode({
@@ -17,7 +27,22 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
       },
     });
   });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
 };
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
